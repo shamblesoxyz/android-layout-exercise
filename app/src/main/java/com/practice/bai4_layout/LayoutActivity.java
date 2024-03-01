74dd996553dccd27630ea632f8fe232e6b8ffb52
Trong Phuc <phuc86119@gmail.com>
2024-03-01 21:58:41 +0700

    init

---
+++ app/src/main/java/com/practice/bai4_layout/LayoutActivity.java
@@ -0,0 +1,14 @@
+package com.practice.bai4_layout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LayoutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.linear_layout_1);
+    }
+}
